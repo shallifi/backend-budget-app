01fece114083b46c60d8e0468d34fc31f2550080
Benjamin <shallifi@hotmail.com>
2022-08-20 21:21:41 -0400

    user resource added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  get "/authorized_user", to: "users#show"
+
     # route to test your configuration
     get '/hello', to: 'application#hello_world'
   # Defines the root path route ("/")
